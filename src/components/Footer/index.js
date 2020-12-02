cf7777ad5c5789183dbea65f46be28435a61010e
Zack White <zhwhite18@yahoo.com>
2020-12-01 23:30:26 -0600

    modified components folder

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,28 +1,29 @@
 import React from "react";
-import Col from "../Col";
 
 function Footer() {
 
     return (
-        <div class="container py-5">
-            <div class="row">
-                <Col size="lg-2">
-                    <p class="font-italic text-muted"></p>
-                    <ul class="list-inline mt-4">
 
-                    </ul>
-                </Col>
+        <footer class="bg-dark">
+                    <div class="container py-5">
+            <div class="row py-4">
+                <div class="col-lg-2" >
 
-                <Col size="lg-6">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Company</h6>
+                </div>
 
-                </Col>
-                <Col size="lg-4">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Where To Find Us</h6>
-                </Col>
+                <div class="col-lg-6">
+                    
+                </div>
+                <div class="col-lg-4">
+
+                </div>
             </div>
         </div>
-    )
+        </footer>
+
+        
+    );
+   
 }
 
 export default Footer;
